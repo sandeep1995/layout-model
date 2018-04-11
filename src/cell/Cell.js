f1fdcade54ced600a8eb42705056f036bc2d71f9
Sandeep Acharya <i.am.sandeep.acharya@gmail.com>
2018-04-11 10:21:23 +0530

    cell dep tree created

--- src/cell/Cell.js
+++ src/cell/Cell.js
@@ -5,6 +5,23 @@ class Cell {
         this.height = height;
         this.position = position;
     }
+
+    updateWidth(width) {
+        this.width = width;
+    }
+
+    updateHeight(height) {
+        this.height = height;
+    }
+
+    resizeTo(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    updatePosition(position) {
+        this.position = position;
+    }
 }
 
 export default Cell;
--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,18 +1,41 @@
 import TreeModel from 'tree-model';
+import Cell from '../cell';
 /* eslint-disable require-jsdoc */
 class Layout {
     constructor(measurements, config) {
         this.measurements = measurements;
         this.config = config;
-        this.tree = this.createTree(this.config);
+
+        this.configTree = this.createConfigTree();
+
+        this.cellDepInitialConfig = {
+            cell: this.getRootCell(),
+            children: []
+        };
+
+        this.cellDepTree = this.createCellDepTree();
     }
 
-    createTree(config) {
+    createConfigTree() {
         return new TreeModel({
             childrenPropertyName: 'lanes'
-        }).parse(config);
+        }).parse(this.config);
     }
 
+    getRootCell() {
+        const position = { top: 0, left: 0 },
+            rootCell = new Cell(
+            this.measurements.width,
+            this.measurements.height,
+            position
+        );
+
+        return rootCell;
+    }
+
+    createCellDepTree() {
+        return new TreeModel().parse(this.cellDepInitialConfig);
+    }
 }
 
 export default Layout;
