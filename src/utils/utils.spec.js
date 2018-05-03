0693bcf03fe1b79df305eb8327f315790b96768c
Sandeep Acharya <i.am.sandeep.acharya@gmail.com>
2018-05-03 19:54:28 +0530

    utils spec added

--- src/utils/utils.spec.js
+++ src/utils/utils.spec.js
@@ -1,5 +1,16 @@
-import { expect } from 'chai';
-import { isEqual, getNodeId } from './utils';
+import {
+  expect
+} from 'chai';
+import {
+  isEqual,
+  getNodeId,
+  yExtraSpace,
+  xExtraSpace
+} from './utils';
+
+import Node from '../tree/';
+import DummyComponent from '../utils/dummy-component';
+import LayoutModel from '../layout-model';
 
 describe('Utils', () => {
     describe('#isEqualStringValue', () => {
@@ -14,7 +25,9 @@ describe('Utils', () => {
         });
 
         it('should throw an error', () => {
-            expect(() => { isEqual({}, VERTICAL); })
+            expect(() => {
+                isEqual({}, VERTICAL);
+            })
                             .to
                             .throw(TypeError, 'value and compareTo must be string');
         });
@@ -23,6 +36,90 @@ describe('Utils', () => {
     describe('#getNodeId', () => {
         it('should create a unique node id', () => {
             expect(getNodeId()).to.be.not.equal(getNodeId());
+        });
+    });
+
+    describe('#yExtraSpace', () => {
+        it('should return the correct vertical extra spcae', () => {
+            const width = 100,
+                height = 100;
+            let comp1 = new DummyComponent(10, {
+                    width,
+                    height: height / 2
+                }),
+                comp2 = new DummyComponent(10, {
+                    width,
+                    height: height / 2
+                }),
+                layoutDef = {
+                    host: null,
+                    ratioWeight: 1,
+                    cut: 'h',
+                    lanes: [{
+                        host: comp1,
+                        ratioWeight: 1,
+                        cut: null,
+                        lanes: []
+                    },
+                    {
+                        host: comp2,
+                        ratioWeight: 1,
+                        cut: null,
+                        lanes: []
+                    }
+                    ]
+                },
+
+                layout = new LayoutModel({
+                    width,
+                    height
+                }, layoutDef),
+
+                root = layout.tree();
+
+            expect(yExtraSpace(root)).to.be.equal(40);
+        });
+    });
+
+    describe('#xExtraSpace', () => {
+        it('should return the correct horizontal extra spcae', () => {
+            const width = 100,
+                height = 100;
+            let comp1 = new DummyComponent(10, {
+                    width: width / 2,
+                    height
+                }),
+                comp2 = new DummyComponent(10, {
+                    width: width / 2,
+                    height
+                }),
+                layoutDef = {
+                    host: null,
+                    ratioWeight: 1,
+                    cut: 'v',
+                    lanes: [{
+                        host: comp1,
+                        ratioWeight: 1,
+                        cut: null,
+                        lanes: []
+                    },
+                    {
+                        host: comp2,
+                        ratioWeight: 1,
+                        cut: null,
+                        lanes: []
+                    }
+                    ]
+                },
+
+                layout = new LayoutModel({
+                    width,
+                    height
+                }, layoutDef),
+
+                root = layout.tree();
+
+            expect(xExtraSpace(root)).to.be.equal(40);
         });
     });
 });
